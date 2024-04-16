213be1a5f9b5bd7b431aae4f3f0991fe7ba8b0a9
Guilherme Victor <55333929+guivictorr@users.noreply.github.com>
2024-04-16 20:16:55 -0300

    feat: defining page layout"

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,22 @@
+import ChatInput from "@/components/chat-input";
+
 export default function Home() {
-  return <h1>Hello World</h1>;
+  return (
+    <div className="flex flex-col">
+      <header className="flex items-center px-8 py-4 h-20 border-b">
+        <h1 className="font-bold text-xl">ChatPDF</h1>
+      </header>
+      <section className="p-4 h-full flex items-center justify-between gap-4">
+        <aside className="hidden md:block border rounded-md flex-1 h-full">
+          pdf preview
+        </aside>
+        <main className="flex flex-col gap-2 flex-1 h-full">
+          <div className="grow border rounded-md">chat</div>
+          <footer className="mt-auto">
+            <ChatInput />
+          </footer>
+        </main>
+      </section>
+    </div>
+  );
 }

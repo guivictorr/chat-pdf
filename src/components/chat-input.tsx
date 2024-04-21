2175da9ce93eb45b7aadef5ec8a862c0789c4ca0
Guilherme Victor <55333929+guivictorr@users.noreply.github.com>
2024-04-21 20:07:47 -0300

    refactor: adjusting chat-input styles

--- src/components/chat-input.tsx
+++ src/components/chat-input.tsx
@@ -1,83 +1,41 @@
 "use client";
 
-import { CornerDownLeft, Mic, Paperclip } from "lucide-react";
+import { CornerDownLeft } from "lucide-react";
 
 import { Button } from "@/components/ui/button";
 import { Label } from "@/components/ui/label";
 import { Textarea } from "@/components/ui/textarea";
-import {
-  Tooltip,
-  TooltipContent,
-  TooltipTrigger,
-  TooltipProvider,
-} from "@/components/ui/tooltip";
-import { ChangeEventHandler, useRef } from "react";
-import { useSetAtom } from "jotai";
-import { fileAtom } from "@/state/file";
+import { useRef } from "react";
 
 export default function ChatInput() {
-  const hiddenInputFile = useRef<HTMLInputElement>(null);
-  const setFile = useSetAtom(fileAtom);
+  const textInputRef = useRef<HTMLTextAreaElement>(null);
 
-  const handleFileChange: ChangeEventHandler<HTMLInputElement> = (event) => {
-    if (event.target.files && event.target.files[0]) {
-      const file = event.target.files[0];
-      setFile(file);
-    }
-  };
-
-  const handleAttachFile = () => {
-    if (!hiddenInputFile.current) return;
-    hiddenInputFile.current.click();
-  };
-
+  function handleTextInputHeight() {
+    if (!textInputRef.current) return;
+    textInputRef.current.style.height = "";
+    textInputRef.current.style.height =
+      textInputRef.current.scrollHeight + "px";
+    textInputRef.current.scrollTop = textInputRef.current.scrollHeight;
+  }
   return (
-    <form className="relative overflow-hidden rounded-lg border bg-background focus-within:ring-1 focus-within:ring-ring hover:border-muted-foreground transition">
+    <form className="relative flex items-center overflow-hidden rounded-lg border bg-background focus-within:ring-1 focus-within:ring-ring hover:border-muted-foreground transition">
       <Label htmlFor="message" className="sr-only">
         Message
       </Label>
       <Textarea
+        ref={textInputRef}
         id="message"
         placeholder="Type your message here..."
-        className="min-h-12 resize-none border-0 p-3 shadow-none focus-visible:ring-0"
+        className="resize-none max-h-[124px] border-0 p-3 shadow-none focus-visible:ring-0 pr-16 text-lg"
+        onChange={handleTextInputHeight}
       />
-      <div className="flex items-center p-3 pt-0">
-        <input
-          onChange={handleFileChange}
-          name="selectedFile"
-          type="file"
-          ref={hiddenInputFile}
-          multiple={false}
-          accept="application/pdf"
-          className="hidden"
-        />
-        <TooltipProvider>
-          <Tooltip>
-            <TooltipTrigger asChild>
-              <Button
-                onClick={handleAttachFile}
-                variant="ghost"
-                size="icon"
-                type="button"
-              >
-                <Paperclip className="size-4" />
-                <span className="sr-only">Attach file</span>
-              </Button>
-            </TooltipTrigger>
-            <TooltipContent side="top">Attach File</TooltipContent>
-          </Tooltip>
-          <Tooltip>
-            <TooltipTrigger asChild>
-              <Button variant="ghost" size="icon">
-                <Mic className="size-4" />
-                <span className="sr-only">Use Microphone</span>
-              </Button>
-            </TooltipTrigger>
-            <TooltipContent side="top">Use Microphone</TooltipContent>
-          </Tooltip>
-        </TooltipProvider>
-        <Button type="submit" size="sm" className="ml-auto gap-1.5">
-          Send Message
+      <div className="absolute h-full right-3 py-2 flex items-end">
+        <Button
+          type="submit"
+          size="sm"
+          className="ml-auto gap-1.5"
+          aria-label="Send"
+        >
           <CornerDownLeft className="size-3.5" />
         </Button>
       </div>

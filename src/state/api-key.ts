a61b7ac3b3701bba7cf6565d0254c84597f34830
Guilherme Victor <55333929+guivictorr@users.noreply.github.com>
2024-04-28 06:12:17 -0300

    refactor: changing the api key name in local storage

--- src/state/api-key.ts
+++ src/state/api-key.ts
@@ -1,3 +1,6 @@
 import { atomWithStorage } from "jotai/utils";
 
-export const apiKeyAtom = atomWithStorage<string | null>("apiKey", null);
+export const apiKeyAtom = atomWithStorage<string | null>(
+  "CHAT_PDF:API_KEY",
+  null,
+);

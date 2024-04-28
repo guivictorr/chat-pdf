c11fd5606c8555fabc2aa8f6269f7e6ed13266f6
Guilherme Victor <55333929+guivictorr@users.noreply.github.com>
2024-04-28 05:04:06 -0300

    refactor: removing useRef from chat input

--- src/components/chat-input.tsx
+++ src/components/chat-input.tsx
@@ -5,29 +5,26 @@ import { CornerDownLeft } from "lucide-react";
 import { Button } from "@/components/ui/button";
 import { Label } from "@/components/ui/label";
 import { Textarea } from "@/components/ui/textarea";
-import { useRef } from "react";
+import { ChangeEvent } from "react";
 
 export default function ChatInput() {
-  const textInputRef = useRef<HTMLTextAreaElement>(null);
-
-  function handleTextInputHeight() {
-    if (!textInputRef.current) return;
-    textInputRef.current.style.height = "";
-    textInputRef.current.style.height =
-      textInputRef.current.scrollHeight + "px";
-    textInputRef.current.scrollTop = textInputRef.current.scrollHeight;
+  function handleMessage(event: ChangeEvent<HTMLTextAreaElement>) {
+    const target = event.currentTarget;
+    target.style.height = "";
+    target.style.height = target.scrollHeight + "px";
+    target.scrollTop = target.scrollHeight;
   }
+
   return (
     <form className="relative flex items-center overflow-hidden rounded-lg border bg-background focus-within:ring-1 focus-within:ring-ring hover:border-muted-foreground transition">
       <Label htmlFor="message" className="sr-only">
         Message
       </Label>
       <Textarea
-        ref={textInputRef}
         id="message"
         placeholder="Type your message here..."
         className="resize-none max-h-[124px] border-0 p-3 shadow-none focus-visible:ring-0 pr-16 text-lg"
-        onChange={handleTextInputHeight}
+        onChange={handleMessage}
       />
       <div className="absolute h-full right-3 py-2 flex items-end">
         <Button

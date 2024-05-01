a725a70c612d8bcb0f0c82c55974b549bf55db9c
Guilherme Victor <55333929+guivictorr@users.noreply.github.com>
2024-04-30 22:28:12 -0300

    feat: adding sonner component from shadcn

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { Toaster } from "@/components/ui/sonner";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { Provider as JotaiProvider } from "jotai";
 import { PropsWithChildren } from "react";
@@ -9,6 +10,7 @@ const queryClient = new QueryClient();
 export default function Providers({ children }: PropsWithChildren) {
   return (
     <JotaiProvider>
+      <Toaster position="top-right" />
       <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
     </JotaiProvider>
   );
